a5fcd43a4ddfc86c12d7457069f5658ee11b30b2
Kebe Eyong <kebe@MacBook-Pro.local>
2022-12-22 16:56:52 +0100

    add ami ids

--- main.tf
+++ main.tf
@@ -40,27 +40,27 @@ resource "aws_dynamodb_table" "terraform_locks" {
 }
 
 
-# Create VPC
+# creating VPC
 module "VPC" {
-  source                                = "./modules/VPC"
-  region                                = var.region
-  vpc_cidr                              = var.vpc_cidr
-  enable_dns_support                    = var.enable_dns_support
-  enable_dns_hostnames                  = var.enable_dns_hostnames
-  enable_classiclink                    = var.enable_classiclink
-  preferred_number_of_public_subnets    = var.preferred_number_of_public_subnets
-  preferred_number_of_private_subnets   = var.preferred_number_of_private_subnets
-  private_subnets                       = [for i in range(1, 8, 2) : cidrsubnet(var.vpc_cidr, 8, i)]
-  public_subnets                        = [for i in range(2, 5, 2) : cidrsubnet(var.vpc_cidr, 8, i)]
+  source                              = "./modules/VPC"
+  region                              = var.region
+  vpc_cidr                            = var.vpc_cidr
+  enable_dns_support                  = var.enable_dns_support
+  enable_dns_hostnames                = var.enable_dns_hostnames
+  enable_classiclink                  = var.enable_classiclink
+  preferred_number_of_public_subnets  = var.preferred_number_of_public_subnets
+  preferred_number_of_private_subnets = var.preferred_number_of_private_subnets
+  private_subnets                     = [for i in range(1, 8, 2) : cidrsubnet(var.vpc_cidr, 8, i)]
+  public_subnets                      = [for i in range(2, 5, 2) : cidrsubnet(var.vpc_cidr, 8, i)]
 }
 
-# Module for Application Load balancer, this will create Extenal Load balancer and internal load balancer
+#Module for Application Load balancer, this will create Extenal Load balancer and internal load balancer
 module "ALB" {
   source             = "./modules/ALB"
   name               = "ACS-ext-alb"
   vpc_id             = module.VPC.vpc_id
-  public-sg          = module.Security.ALB-sg
-  private-sg         = module.Security.IALB-sg
+  public-sg          = module.security.ALB-sg
+  private-sg         = module.security.IALB-sg
   public-sbn-1       = module.VPC.public_subnets-1
   public-sbn-2       = module.VPC.public_subnets-2
   private-sbn-1      = module.VPC.private_subnets-1
@@ -69,7 +69,7 @@ module "ALB" {
   ip_address_type    = "ipv4"
 }
 
-module "Security" {
+module "security" {
   source = "./modules/Security"
   vpc_id = module.VPC.vpc_id
 }
@@ -77,12 +77,12 @@ module "Security" {
 
 module "AutoScaling" {
   source            = "./modules/Autoscaling"
-  ami-web           = var.ami
-  ami-bastion       = var.ami
-  ami-nginx         = var.ami
-  desired_capacity  = 2
-  min_size          = 2
-  max_size          = 2
+  ami-web           = var.ami-web
+  ami-bastion       = var.ami-bastion
+  ami-nginx         = var.ami-nginx
+  desired_capacity  = 1
+  min_size          = 1
+  max_size          = 1
   web-sg            = [module.security.web-sg]
   bastion-sg        = [module.security.bastion-sg]
   nginx-sg          = [module.security.nginx-sg]
@@ -97,7 +97,7 @@ module "AutoScaling" {
 }
 
 # Module for Elastic Filesystem; this module will creat elastic file system isn the webservers availablity
-# zone and allow traffic from the webservers
+# zone and allow traffic fro the webservers
 
 module "EFS" {
   source       = "./modules/EFS"
@@ -120,10 +120,10 @@ module "RDS" {
 # The Module creates instances for jenkins, sonarqube abd jfrog
 module "compute" {
   source          = "./modules/compute"
-  ami-jenkins     = var.ami
-  ami-sonar       = var.ami
-  ami-jfrog       = var.ami
+  ami-jenkins     = var.ami-bastion
+  ami-sonar       = var.ami-sonar
+  ami-jfrog       = var.ami-bastion
   subnets-compute = module.VPC.public_subnets-1
-  sg-compute      = [module.security.ALB-sg]
+  sg-compute      = [module.security.compute-sg]
   keypair         = var.keypair
 }
