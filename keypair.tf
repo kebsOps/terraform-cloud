a4d0c345a3e7d44b2b29fa95e2c4b68e6df447a3
Kebe Eyong <kebe@MacBook-Pro.local>
2022-12-22 15:46:13 +0100

    add ami ids

--- keypair.tf
+++ keypair.tf
@@ -1,4 +1,4 @@
- resource "aws_key_pair" "devopspbl" {
-  key_name   = "id_rsa.pub"
-  public_key = file("/id_rsa.pub")
-}
+ #resource "aws_key_pair" "devopspbl" {
+  #key_name   = "id_rsa.pub"
+  #public_key = file("/id_rsa.pub")
+#}
