ee1929572649457133d1e1320e38eca434409f86
Kebe Eyong <kebe@MacBook-Pro.local>
2022-12-29 06:18:29 +0100

    some modifications

--- main.tf
+++ main.tf
@@ -3,27 +3,30 @@
 #############################
 
 # Note: The bucket name may not work for you since buckets are unique globally in AWS, so you must give it a unique name.
-####}
+resource "aws_s3_bucket" "terraform_state" {
+  bucket        = "kebe-dev-terraform-bucket"
+  force_destroy = true
+}
 
 # Enable versioning so we can see the full revision history of our state files
-#resource "aws_s3_bucket_versioning" "version" {
- # bucket = aws_s3_bucket.terraform-state.id
-  #versioning_configuration {
-   # status = "Enabled"
-  #}
-#}
+resource "aws_s3_bucket_versioning" "version" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
 
 
 
 # Enable server-side encryption by default
-#resource "aws_s3_server_side_encryption_configuration" "terraform_state" {
-  #bucket = aws_s3_bucket.terraform_state.id
-  #rule {
-    #apply_server_side_encryption_by_default {
-   #   sse_algorithm = "AES256"
- #   }
-  #}
-#}
+resource "aws_s3_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
 
 
 resource "aws_dynamodb_table" "terraform_locks" {
