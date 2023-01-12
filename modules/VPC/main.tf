930d844a24cb15d5bddb9bc5d1fcbd3a2d5dc240
kebsOps <kebeeyong@gmail.com>
2023-01-12 22:22:42 +0100

    some changes

--- modules/VPC/main.tf
+++ modules/VPC/main.tf
@@ -24,12 +24,11 @@ data "aws_availability_zones" "available" {
 
 # Create public subnets
 resource "aws_subnet" "public" {
-  count                   = var.preferred_number_of_public_subnets == null ? length(data.aws_availability_zones.available.names) : var.preferred_number_of_public_subnets
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.public_subnets[count.index]
-  map_public_ip_on_launch = true
-  availability_zone       = data.aws_availability_zones.available.names[count.index]
-
+    count                   = var.preferred_number_of_public_subnets
+    vpc_id                  = aws_vpc.main.id
+    cidr_block              = cidrsubnet(var.vpc_cidr, 4 , count.index)
+    map_public_ip_on_launch = true
+    availability_zone       = data.aws_availability_zones.available.names[count.index]
 
 
   tags = merge(
@@ -43,11 +42,11 @@ resource "aws_subnet" "public" {
 
 # Create private subnets
 resource "aws_subnet" "private" {
-  count                   = var.preferred_number_of_private_subnets == null ? length(data.aws_availability_zones.available.names) : var.preferred_number_of_private_subnets
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.private_subnets[count.index]
-  map_public_ip_on_launch = true
-  availability_zone       = data.aws_availability_zones.available.names[count.index]
+      count                   = var.preferred_number_of_private_subnets
+    vpc_id                  = aws_vpc.main.id
+    cidr_block              = cidrsubnet(var.vpc_cidr, 4 , count.index+2)
+    map_public_ip_on_launch = false
+    availability_zone       = data.aws_availability_zones.available.names[count.index]
 
   tags = merge(
     var.tags,
