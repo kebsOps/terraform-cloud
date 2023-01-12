5c3a6a37ee09acd7b88c240871b99301609f939a
kebsOps <kebeeyong@gmail.com>
2023-01-12 22:33:28 +0100

    some changes

--- modules/VPC/main.tf
+++ modules/VPC/main.tf
@@ -1,10 +1,5 @@
-
-
-
-
-
 # Create VPC
-resource "aws_vpc" "main" {
+resource "aws_default_vpc" "main" {
   cidr_block                     = var.vpc_cidr
   enable_dns_support             = var.enable_dns_support
   enable_dns_hostnames           = var.enable_dns_hostnames
