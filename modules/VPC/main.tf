69eeee324434f817b913caba0280ae90c89e1a0f
kebsOps <kebeeyong@gmail.com>
2023-01-12 22:27:50 +0100

    some changes

--- modules/VPC/main.tf
+++ modules/VPC/main.tf
@@ -43,7 +43,7 @@ resource "aws_subnet" "private" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = cidrsubnet(var.vpc_cidr, 8, count.index + 2)
   map_public_ip_on_launch = true
-  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  availability_zone       = element(data.aws_availability_zones.available.names, count.index)
   tags = merge(
     var.tags,
     {
